35fbd71ee97e8ee2b6b756fa172e7e4b0bd01b7f
Hassia T <hassia.tamimou@gmail.com>
2019-11-30 13:03:59 +0100

    adding feature :saving file before deletion

--- Bashscripting/commands.sh
+++ Bashscripting/commands.sh
@@ -1,22 +1,42 @@
 #!/bin/bash
-
 #defining alias for rm command
+#defining global variables
+GLOBALSOURCE=0
+
+copysafe() {
+SOURCE=$GLOBALSOURCE
+echo "Starting copying file in argument"
+echo "The file " "$SOURCE" " will be saved"
+currentDate=$(date '+%Y-%m-%d')
+echo today\'s date: "$currentDate"
+DIRECTORY=~/.deleted/$currentDate/
+if [ ! -d "$DIRECTORY"  ]
+then 
+	echo "the directory does not exist. Creating it...."	
+	mkdir ~/.deleted/$currentDate
+fi
+DESTINATION=/home/htsme/.deleted/"$currentDate"/"$SOURCE"
+cp -R "$SOURCE" "$DESTINATION"
+echo ">>> File saved before deletion"
+}
 
 
 rmsafe () {
+	NAMEFILE=$1
+	echo "$NAMEFILE" "HERE"
+	GLOBALSOURCE="$NAMEFILE"
+	echo "$GLOBALSOURCE"
+	copysafe $GLOBALSOURCE
 	echo "You are about to do something dangerous"
 	ISDIR=false	
-	NAMEFILE=$1
 	MESSAGE="Are you sure to erase "
 	MARK="?"
-	WHOLE=$MESSAGE$NAMEFILE$MARK
-		
+	WHOLE=$MESSAGE$NAMEFILE$MARK	
 	if [ -d "$NAMEFILE" ]
 	then isDirectory =true; echo "You are trying to delete DIRECTORY :""$NAMEFILE"
 	else
 		echo "You want to delete FILE:""$NAMEFILE"
-	fi
-	
+	fi	
 	echo "$WHOLE"
 	read -p "Please choose yes or no: " -n 1 -r
 	echo 	
